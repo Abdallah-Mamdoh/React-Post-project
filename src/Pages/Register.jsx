cbda21a9501948a5930891f8c88ed96eb457b6fb
Abdallah <abdallahmamdouh.eng@gmail.com>
2024-07-30 18:30:57 +0300

    add projrct files

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -26,7 +26,7 @@ export default function Register({form,errors,handleChange,handleConfirmation,ha
    
   return (
     <div>
-      <section className={`bg-gray-50 dark:bg-gray-900 pt-16 pb-5 ${classes[0]}`}>
+      <section className={`bg-gray-50 dark:bg-gray-900 pt-32 pb-20 ${classes[0]}`}>
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
